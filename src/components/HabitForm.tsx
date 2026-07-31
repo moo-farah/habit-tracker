aa6c998ad1668739c9ea9f8556437d927e0be556
moo-farah <rizmik.farah@gmail.com>
2026-07-31 23:57:11 +0300

    fix: small bug on HabitForm

--- src/components/HabitForm.tsx
+++ src/components/HabitForm.tsx
@@ -3,7 +3,7 @@ import { Button } from "./Button"
 const HabitForm = () => {
   return (
     <form className="flex gap-2">
-        <input className="flex-1 rounded-lg bg-[#292929] text-[#ffffff] px-4 py-2 outline-none focus-visible:ring-2 focus-visible:ring-[#5B6F00]"
+        <input className="flex-1 rounded-lg bg-foreground text-white px-4 py-2 outline-none focus-visible:ring-2 focus-visible:ring-[#5B6F00]"
         placeholder="New habit..."
         />
         <Button>Add Habit</Button>
